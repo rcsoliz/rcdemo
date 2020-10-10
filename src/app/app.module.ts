6c4a6e6be3816b0ede38a8b82150658cd6f7e9ca
Roberto Soliz <robertoucbnh@gmail.com>
2020-10-10 07:58:11 -0400

    message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponet } from './products/product.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DetailprodutComponent } from './products/detailprodut/detailprodut.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponet,
+    HomeComponent,
+    NotFoundComponent,
+    DetailprodutComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
